c28449ef54ba610e161a153c4af4be7184de65ae
jhiney <hiney.jake@outlook.com>
2022-05-18 09:11:30 -0500

    Return and handle an error

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -1,8 +1,18 @@
 package greetings
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
-func Hello(name string) string {
+//hello returns a greeting for the named person
+func Hello(name string) (string, error) {
+	//if no name was given, return an error with a message
+	if name == "" {
+		return "", errors.New("no name")
+	}
+	//If a name was received, return a value that embeds the name
+	//in a message
 	message := fmt.Sprintf("Hi, %v. Welcome!", name)
-	return message
+	return message, nil
 }
